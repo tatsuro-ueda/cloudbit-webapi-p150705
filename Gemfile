d6f08c647162a1b665ea620853914ba4d0f6e42f
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2015-07-05 13:29:41 +0900

    ➕基本機能

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'sinatra'
+gem 'rest-client'
+gem 'shotgun'
+
+ruby '2.2.0'
---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+require 'rest-client'
+
+class Server < Sinatra::Base
+  get '/' do
+    RestClient.get \
+      "https://api-http.littlebitscloud.cc/devices/#{ENV["THERMO_CLOUDBIT_DEVICE_ID"]}", 
+      Accept: "application/vnd.littlebits.v2+json", 
+      Authorization: "Bearer #{ENV["THERMO_CLOUDBIT_TOKEN"]}"
+  end 
+end
