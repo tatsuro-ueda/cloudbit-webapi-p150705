6d506178f9bfda64fb3711731c93d2db06d66fd8
Ayumu AIZAWA <ayumu.aizawa@gmail.com>
2015-07-07 14:24:39 +0900

    fix quotation

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require 'rest-client'
 class Server < Sinatra::Base
   get '/' do
     RestClient.get \
-      "https://api-http.littlebitscloud.cc/devices/#{ENV["THERMO_CLOUDBIT_DEVICE_ID"]}", 
-      Accept: "application/vnd.littlebits.v2+json", 
-      Authorization: "Bearer #{ENV["THERMO_CLOUDBIT_TOKEN"]}"
+      "https://api-http.littlebitscloud.cc/devices/#{ENV['THERMO_CLOUDBIT_DEVICE_ID']}",
+      Accept: "application/vnd.littlebits.v2+json",
+      Authorization: "Bearer #{ENV['THERMO_CLOUDBIT_TOKEN']}"
   end 
 end
