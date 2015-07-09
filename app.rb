4fca1ea2985acbb3438a11929e929c884fe83325
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2015-07-09 15:32:48 +0900

    🚀デバイスIDとアクセストークンを、リクエストの中で指定できるようにした

--- app.rb
+++ app.rb
@@ -5,17 +5,16 @@ require 'sinatra/cross_origin'
 class Server < Sinatra::Base
   register Sinatra::CrossOrigin
   enable :cross_origin
-  # cross_origin :allow_origin => 'http://swagger.io'
 
-  uri = URI(
-    "https://api-http.littlebitscloud.cc/devices/#{ENV['THERMO_CLOUDBIT_DEVICE_ID']}/input"
-  )
+  def requestCloudbitCondition()
+    uri = URI(
+      "https://api-http.littlebitscloud.cc/devices/#{params['THERMO_CLOUDBIT_DEVICE_ID']}/input"
+    )
 
-  request = Net::HTTP::Get.new uri
-  request['Accept'] = "application/vnd.littlebits.v2+json"
-  request['Authorization'] = "Bearer #{ENV['THERMO_CLOUDBIT_TOKEN']}"
+    request = Net::HTTP::Get.new uri
+    request['Accept'] = "application/vnd.littlebits.v2+json"
+    request['Authorization'] = "Bearer #{params['THERMO_CLOUDBIT_ACCESS_TOKEN']}"
 
-  get '/' do
     Net::HTTP.start(uri.host, uri.port,
       :use_ssl => uri.scheme == 'https') do |http|
       http.request request do |response|
@@ -24,5 +23,9 @@ class Server < Sinatra::Base
         end
       end
     end
+  end
+
+  get '/input' do
+    requestCloudbitCondition()
   end
 end
